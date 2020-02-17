dc471f60714f5a0eff6dc983b89bee3743f94463
Reynevar <rey@localhost.localdomain>
2020-02-17 21:36:44 +0100

    Updates

---
+++ precache-manifest.eef0c0a3c46822ef473fe3ac67c4bc6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c04154c21fc419235d90f206da9a961",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a5d96305ea8148859a05",
+    "url": "/react-tutorial/static/css/main.119d0431.chunk.css"
+  },
+  {
+    "revision": "e257cc4ac4564e43f115",
+    "url": "/react-tutorial/static/js/2.29478003.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tutorial/static/js/2.29478003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d96305ea8148859a05",
+    "url": "/react-tutorial/static/js/main.cba1106a.chunk.js"
+  },
+  {
+    "revision": "d1a4ad59d776d93e8cfb",
+    "url": "/react-tutorial/static/js/runtime-main.4b063f4c.js"
+  }
+]);
